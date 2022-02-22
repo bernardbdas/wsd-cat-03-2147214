d37ab53e4061087084295e9b0d3c933741968f3b
bernardbdas <bernarddas1@gmail.com>
2022-02-23 00:29:03 +0530

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,16 +7,19 @@ app.use(parser.json());
 var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "",
+    //password: "",
     database: ""
 });
+
 connection.connect((err) => {
     if (!err)
         console.log("Database is connected!");
     else
         console.log("CONNECTION ERROR!");
 })
+
 app.listen(5200, () => console.log('server has been started!'));
+
 app.get("/info", (req, res) => {
     connection.query("SELECT * from stuinfo", (err, rows, fields) => {
         if (!err)
@@ -25,6 +28,7 @@ app.get("/info", (req, res) => {
             console.log("error");
     })
 })
+
 app.get('/add', (_req, res) => {
     var post = { stu_id: 111, stu_name: 'Wix', age: 22, gender: "Female", course: "MCA", address: "Bangalore", grade: "A" };
     var sql = 'INSERT INTO stuinfo SET ?';
@@ -33,6 +37,7 @@ app.get('/add', (_req, res) => {
         res.send("Inserted Rows...");
     })
 });
+
 app.get('/update/id', (req, res) => {
     var name = 'Archu'
     var sql = `UPDATE stuinfo SET stu_name='${name}' WHERE stu_id= '${req.params.id}'`;
@@ -41,6 +46,7 @@ app.get('/update/id', (req, res) => {
         res.send("Successfully Updated...");
     })
 });
+
 app.get('/delete/id', (req, res) => {
     var sql = `Delete from stuinfo WHERE stu_id= '${req.params.id}'`;
     var query = connection.query(sql, (err, result) => {
